16f4ee492e391643406cef159d3eb3d80e8282f0
bookishgallifreyan <05stanforthw@googlemail.com>
2024-06-25 14:53:32 +0100

    weird-punctuation

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1>This is my website! It's not all that good, I admit...</h1>
+      <h1>This is my website! It&apos;s not all that good, I admit...</h1>
       <p>
         This is a test website so I can mess about with a bit of styling and
         trying to deploy it online. If found while surfing, please ignore.
